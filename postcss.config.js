86ba52099f55016353ef5c154b71c82ae5730722
Ahmad Husen <hapakaien@gmail.com>
2021-04-27 05:33:16 +0700

    feat: add import & nested into postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+const postcssImport = require('postcss-import');
+const postcssNested = require('postcss-nested');
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
@@ -7,7 +9,9 @@ const prod = mode === 'production';
 
 module.exports = {
   plugins: [
+    postcssImport(),
     tailwindcss(),
+    postcssNested(),
     autoprefixer(),
 
     prod &&
